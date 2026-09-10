049e8364782a11b60acdd4fdf50b407d1b06b912
Md Alamin <developeralaminvi@gmail.com>
2026-09-11 01:29:48 +0600

    chore: exclude large zips from git and add standalone builder

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'standalone',
   images: {
     domains: ['images.unsplash.com', 'graph.facebook.com', 'platform-lookaside.fbsbx.com']
   }
